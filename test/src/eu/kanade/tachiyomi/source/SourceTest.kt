4e2c38241b8bbed9a05c36709bc060c2e47e41ce
J2ghz <j2.00ghz@gmail.com>
2017-06-03 19:46:42 +0200

    set up testing

---
+++ test/src/eu/kanade/tachiyomi/source/SourceTest.kt
@@ -0,0 +1,14 @@
+package eu.kanade.tachiyomi.source
+
+import org.assertj.core.api.Assertions.assertThat
+class SourceTest {
+    @org.junit.Before
+    fun Setup(){
+
+    }
+    @org.junit.Test
+    fun Test(){
+        assertThat(true).isTrue()
+
+    }
+}
